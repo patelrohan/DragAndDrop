5f576a8f065fd4c04ba5c51bfff8be400f7ae4df
Rohan Patel <patelrohan@users.noreply.github.com>
2023-08-03 19:10:44 -0400

    Basic drag and drop done
    - Added Algorithms package
    - Basic implementation of drag and drop string based tasks done

--- DragAndDrop/ContentView.swift
+++ DragAndDrop/ContentView.swift
@@ -6,18 +6,76 @@
 //
 
 import SwiftUI
+import Algorithms
 
 struct ContentView: View {
     
     @State private var toDoTasks: [String] = ["Make Dinner","Leetcode Problem","Meditation"]
     @State private var inProgressTasks: [String] = ["SwiftUI Tutorial"]
     @State private var doneTasks: [String] = []
+   
+    @State private var isToDoTargeted = false
+    @State private var isInProgressTargeted = false
+    @State private var isDoneTargeted = false
     
     var body: some View {
+        // Display 3 columns of Kanban views
         HStack(spacing: 12){
-            KanbanView(title: "To Do", tasks: toDoTasks)
-            KanbanView(title: "In Progress", tasks: inProgressTasks)
-            KanbanView(title: "Done", tasks: doneTasks)
+            KanbanView(title: "To Do", tasks: toDoTasks, isTargeted: isToDoTargeted)
+                .dropDestination(for:String.self) { droppedTasks, location in
+                    //code for successfull drop
+                    for task in droppedTasks{
+                        inProgressTasks.removeAll{ $0 == task }
+                        doneTasks.removeAll{ $0 == task }
+                    }
+                    
+                    toDoTasks += droppedTasks
+                    //prevent duplication of dropped tasks using uniques() method from Algorithms package. Can use Set instead of array as well
+                    toDoTasks = Array(toDoTasks.uniqued())
+                    
+                    return true
+                    
+                }isTargeted: { isTargeted in
+                    isToDoTargeted = isTargeted
+                }
+            
+            KanbanView(title: "In Progress", tasks: inProgressTasks, isTargeted: isInProgressTargeted)
+                .dropDestination(for:String.self) { droppedTasks, location in
+                    //code for successfull drop
+                    for task in droppedTasks{
+                        toDoTasks.removeAll{ $0 == task }
+                        doneTasks.removeAll{ $0 == task }
+                    }
+                    
+                    inProgressTasks += droppedTasks
+                    //prevent duplication of dropped tasks using uniques() method from Algorithms package. Can use Set instead of array as well
+                    inProgressTasks = Array(inProgressTasks.uniqued())
+                    
+                    return true
+                    
+                }isTargeted: { isTargeted in
+                    isInProgressTargeted = isTargeted
+                }
+            
+            
+            KanbanView(title: "Done", tasks: doneTasks, isTargeted: isDoneTargeted)
+                .dropDestination(for:String.self) { droppedTasks, location in
+                    //code for successfull drop
+                    for task in droppedTasks{
+                        toDoTasks.removeAll{ $0 == task }
+                        inProgressTasks.removeAll{ $0 == task }
+                    }
+                    
+                    doneTasks += droppedTasks
+                    //prevent duplication of dropped tasks using uniques() method from Algorithms package. Can use Set instead of array as well
+                    doneTasks = Array(doneTasks.uniqued())
+                    
+                    return true
+                    
+                }isTargeted: { isTargeted in
+                    isDoneTargeted = isTargeted
+                }
+            
         }
         .padding()
     }
@@ -30,10 +88,12 @@ struct ContentView_Previews: PreviewProvider {
     }
 }
 
+// Setup custom view for Kanban
 struct KanbanView: View{
     
     let title: String
     let tasks: [String]
+    let isTargeted: Bool
     
     var body: some View{
         VStack(alignment: .leading){
@@ -42,7 +102,7 @@ struct KanbanView: View{
             ZStack{
                 RoundedRectangle(cornerRadius: 12)
                     .frame(maxWidth: .infinity)
-                    .foregroundColor(Color(.secondarySystemFill))
+                    .foregroundColor(isTargeted ? .teal.opacity(0.2) : Color(.secondarySystemFill))
                 
                 VStack(alignment: .leading, spacing: 12){
                     ForEach(tasks, id: \.self){ task in
@@ -51,6 +111,7 @@ struct KanbanView: View{
                             .background(Color(uiColor: .secondarySystemGroupedBackground))
                             .cornerRadius(8)
                             .shadow(radius: 1, x: 1, y: 1)
+                            .draggable(task)
                     }
                     Spacer()
                 }
