3b4379a4308b74cd29412bf95d72405b32e3b2e6
Rohan Patel <patelrohan@users.noreply.github.com>
2023-08-03 15:04:13 -0400

    Initial Commit

---
+++ DragAndDrop/DragAndDropApp.swift
@@ -0,0 +1,17 @@
+//
+//  DragAndDropApp.swift
+//  DragAndDrop
+//
+//  Created by Rohan on 8/3/23.
+//
+
+import SwiftUI
+
+@main
+struct DragAndDropApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
